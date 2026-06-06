3db23c67a2796670880ee57a9a6c6cf000d49372
Kishan Doshi <kishan.d@reddotdistribution.com>
2026-06-06 17:05:07 +0300

    feat: update RoomHeader component to remove redundant text in points display

--- apps/web/src/pages/room/components/RoomHeader.tsx
+++ apps/web/src/pages/room/components/RoomHeader.tsx
@@ -43,7 +43,7 @@ function RoomHeader({ room }: Readonly<Props>) {
 						className={`text-accent text-base cursor-default`}
 					>
 						<CoinsIcon />
-						{(player?.points_balance ?? 0).toLocaleString()} pts
+						{(player?.points_balance ?? 0).toLocaleString()}
 					</Button>
 				</div>
 			</div>
